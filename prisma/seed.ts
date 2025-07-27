b6e769d4618aa42cf56166ae0e5bc706cc441aca
marina eleshaa <marinaeleshaa>
2025-07-27 22:13:23 +0300

    update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,30 @@
-import { faker } from "@faker-js/faker";
-import { PrismaClient } from "@prisma/client";
+// import { faker } from "@faker-js/faker";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-  // 2. Seed todos with user_id
-  await prisma.todo.createMany({
-    data: Array.from({ length: 25 }, () => ({
-      title: faker.string.alpha({ length: { min: 6, max: 20 } }),
-      body: faker.lorem.words(80),
-      user_id: faker.database.mongodbObjectId(),
-      // ✅ required field
-      completed: false,
-    })),
-  });
-}
+// async function main() {
+//   // 2. Seed todos with user_id
+//   await prisma.todo.createMany({
+//     data: Array.from({ length: 25 }, () => ({
+//       title: faker.string.alpha({ length: { min: 6, max: 20 } }),
+//       body: faker.lorem.words(80),
+//       user_id: faker.database.mongodbObjectId(),
+//       // ✅ required field
+//       completed: false,
+//     })),
+//   });
+// }
 
-main()
-  .then(() => {
-    console.log("Seeding done");
-    prisma.$disconnect();
-  })
-  .catch((err) => {
-    console.error(err);
-    prisma.$disconnect();
-    process.exit(1);
-  });
+// main()
+//   .then(() => {
+//     console.log("Seeding done");
+//     prisma.$disconnect();
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     prisma.$disconnect();
+//     process.exit(1);
+//   });
 
-// ! npx prisma db seed => Run this command to seed the database
+// // ! npx prisma db seed => Run this command to seed the database
